3872a91b504f02f39fe44d89f54f13e6400e320b
osego <60160526+osego@users.noreply.github.com>
2021-04-22 09:26:16 +0200

    feat: ability to cancel edits, decoupled logic from displayed id

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
-/*Projekt wykonali Mateusz Matowicki, Bartosz Wiszowaty */
-let is_tombstone = (entry) => {
-    return !(entry && Object.keys(entry).length !== 0);
-}
+/* Projekt wykonali Mateusz Matowicki, Bartosz Wiszowaty */
 
 let receiptData;
+let selectedRow = null;
+
 if ((receiptData = window.localStorage["receiptData"])) {
     receiptData = JSON.parse(receiptData);
-    receiptData.forEach((entry) => {
-        if (!is_tombstone(entry)) {
-            insertNewRecord(entry);
-        }
-    })
+    receiptData.forEach((entry) => insertNewRecord(entry));
 } else {
     receiptData = [];
 }
 
-let updateSum = () => {
+let updateGeneratedValues = () => {
     let sumElement = document.getElementById("receipt-sum");
     sumElement.innerHTML = receiptData.reduce((a, b) => a + +(b.sum ?? 0), 0) + " zł";
+    document.querySelectorAll("#product-list tbody:first-of-type tr").forEach((element, index) => {
+        element.cells[0].innerHTML = String(index + 1);
+    });
 }
 
-updateSum();
-let selectedRow = null
+updateGeneratedValues();
+
 function onFormSubmit() {
     let formData = readFormData();
 
     if (selectedRow == null) {
-        formData.id = receiptData.length + 1;
         insertNewRecord(formData);
         receiptData.push(formData);
     } else {
-        let id = formData.id = +selectedRow.cells[0].innerHTML;
         updateRecord(formData);
-        receiptData[receiptData.findIndex((element) => element.id === id)] = formData;
+        receiptData[selectedRow.rowIndex - 1] = formData;
     }
 
-    updateSum();
+    updateGeneratedValues();
     resetForm();
     window.localStorage["receiptData"] = JSON.stringify(receiptData);
 }
 
 function readFormData() {
     let price = +parseFloat(document.getElementById("price").value).toFixed(2);
-    let quantity = +parseFloat(document.getElementById("quantity").value).toFixed(2)
+    let quantity = +parseFloat(document.getElementById("quantity").value).toFixed(2);
     return {
         "name": document.getElementById("name").value,
         "quantity": quantity,
@@ -55,10 +51,10 @@ function insertNewRecord(data, rowIndex) {
     let table = document.getElementById("product-list").getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(rowIndex);
 
-    let cell = newRow.insertCell(0);
-    cell.innerHTML = data.id ?? (receiptData.length + 1);
+    // cell reserved for id, filled when updateGeneratedValues is called
+    newRow.insertCell(0);
 
-    cell = newRow.insertCell(1);
+    let cell = newRow.insertCell(1);
     cell.innerHTML = data.name;
 
     cell = newRow.insertCell(2);
@@ -77,13 +73,16 @@ function insertNewRecord(data, rowIndex) {
 
 function resetForm() {
     selectedRow = null;
+    document.forms["newEntry"].reset();
+    document.getElementById("reset-button").hidden = true;
 }
 
 function onEdit(td) {
+    document.getElementById("reset-button").hidden = false;
     selectedRow = td.parentElement.parentElement;
     document.getElementById("name").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("quantity").value = selectedRow.cells[2].innerHTML.match(/[+-]?\d+(\.\d+)?/g)[0];
-    document.getElementById("price").value = selectedRow.cells[3].innerHTML.match(/[+-]?\d+(\.\d+)?/g)[0];
+    document.getElementById("quantity").value = parseFloat(selectedRow.cells[2].innerHTML);
+    document.getElementById("price").value = parseFloat(selectedRow.cells[3].innerHTML);
 }
 
 function updateRecord(formData) {
@@ -96,33 +95,40 @@ function updateRecord(formData) {
 function onDelete(td) {
     if (confirm('Are you sure to delete this record ?')) {
         let row = td.parentElement.parentElement;
-        receiptData[receiptData.findIndex((element) => element.id === +row.cells[0].innerHTML)] = {};
+        receiptData.splice(+row.cells[0].innerHTML - 1, 1);
         window.localStorage["receiptData"] = JSON.stringify(receiptData);
         document.getElementById("product-list").deleteRow(row.rowIndex);
-        updateSum();
+        updateGeneratedValues();
         resetForm();
     }
 }
 
-let reorder = (td, direction_up) => {
+let reorder = (td, direction) => {
+    // direction is 1 for moving up and 0 for moving down
     let row = td.parentElement.parentElement;
-    let originalPosition = receiptData.findIndex((element) => element.id === +row.cells[0].innerHTML)
-    let sp = originalPosition + (direction_up ? -1 : 1);
-    for (; sp < receiptData.length && sp >= 0 && is_tombstone(receiptData[sp]); (direction_up ? --sp : ++sp)) { }
-    // do nothing if there are no non-tombstone elements above/below what we want to move
+    let position = row.rowIndex - 1;
+    let sp = position + (direction ? -1 : 1);
+
+    // do nothing if there are no elements above/below what we want to move
     if (sp === -1 || sp === receiptData.length) {
         return;
     }
-    receiptData.splice(sp, 0, receiptData.splice(originalPosition, 1)[0]);
+
+    let setNew = (row === selectedRow);
+    receiptData.splice(sp, 0, receiptData.splice(position, 1)[0]);
     window.localStorage["receiptData"] = JSON.stringify(receiptData);
+
     let data = {
-        "id": row.cells[0].innerHTML,
         "name": row.cells[1].innerHTML,
         "quantity": row.cells[2].innerHTML,
         "price": parseFloat(row.cells[3].innerHTML),
         "sum": parseFloat(row.cells[4].innerHTML),
     }
-    let index = row.rowIndex;
-    document.getElementById("product-list").deleteRow(index);
-    insertNewRecord(data, index + (direction_up ? -2 : 0));
+
+    document.getElementById("product-list").deleteRow(position + 1);
+    insertNewRecord(data, position + (direction ? -1 : 1));
+    if (setNew) {
+        selectedRow = document.getElementById("product-list").rows[position + (direction ? 0 : 2)];
+    }
+    updateGeneratedValues();
 }
